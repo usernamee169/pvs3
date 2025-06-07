d92d26a36103dfb0a0742e0a989e435f33479ce7
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-07 18:44:07 +0500

    Update parallel_sum.c

--- parallel_sum.c
+++ parallel_sum.c
@@ -1,60 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <mpi.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-
+    
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+    
     if (argc != 2) {
-        if (rank == 0) printf("Usage: %s <array_size>\n", argv[0]);
-        MPI_Finalize();
-        return 1;
-    }
-
-    int arr_size = atoi(argv[1]);
-    if (arr_size <= 0) {
-        if (rank == 0) printf("Array size must be positive\n");
-        MPI_Finalize();
-        return 1;
-    }
-
-    int *arr = NULL;
-    if (rank == 0) {
-        arr = (int *)malloc(arr_size * sizeof(int));
-        srand(time(NULL));
-        for (int i = 0; i < arr_size; i++) {
-            arr[i] = rand() % 1000;
+        if (rank == 0) {
+            printf("Usage: mpirun -np <processes> %s <array_size>\n", argv[0]);
         }
+        MPI_Finalize();
+        return 1;
     }
-
-    double start = MPI_Wtime();
-    int local_size = arr_size / size;
-    int *local_arr = (int *)malloc(local_size * sizeof(int));
-
-    MPI_Scatter(arr, local_size, MPI_INT, local_arr, local_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    long long local_sum = 0;
-    for (int i = 0; i < local_size; i++) {
-        local_sum += local_arr[i];
-    }
-
-    long long global_sum;
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    double end = MPI_Wtime();
-
+    
+    int array_size = atoi(argv[1]);
+    int* array = NULL;
+    double start_time, end_time;
+    
     if (rank == 0) {
-        printf("Array sum: %lld\n", global_sum);
-        printf("Time taken: %f seconds\n", end - start);
-        free(arr);
+        array = (int*)malloc(array_size * sizeof(int));
+        srand(time(NULL));
+        for (int i = 0; i < array_size; i++) {
+            array[i] = rand() % 100;
+        }
+        start_time = MPI_Wtime();
     }
-
-    free(local_arr);
+    
+    // Распределяем данные по процессам
+    int local_size = array_size / size;
+    int* local_array = (int*)malloc(local_size * sizeof(int));
+    
+    MPI_Scatter(array, local_size, MPI_INT, local_array, local_size, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    // Локальное суммирование
+    int local_sum = 0;
+    for (int i = 0; i < local_size; i++) {
+        local_sum += local_array[i];
+    }
+    
+    // Сбор результатов
+    int global_sum;
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    if (rank == 0) {
+        // Обработка оставшихся элементов
+        int remaining = array_size % size;
+        for (int i = array_size - remaining; i < array_size; i++) {
+            global_sum += array[i];
+        }
+        
+        end_time = MPI_Wtime();
+        printf("Array size: %d\n", array_size);
+        printf("Total sum: %d\n", global_sum);
+        printf("Execution time: %.6f seconds\n", end_time - start_time);
+        free(array);
+    }
+    
+    free(local_array);
     MPI_Finalize();
     return 0;
 }
