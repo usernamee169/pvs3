c244490481a5305fb64e971d0a711be2a5a8f124
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-07 17:57:47 +0500

    Update serial_sum.c

--- serial_sum.c
+++ serial_sum.c
@@ -4,39 +4,32 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Использование: %s <размер_массива>\n", argv[0]);
+        printf("Usage: %s <array_size>\n", argv[0]);
         return 1;
     }
 
     int size = atoi(argv[1]);
     if (size <= 0) {
-        fprintf(stderr, "Размер массива должен быть положительным целым числом.\n");
+        printf("Array size must be positive\n");
         return 1;
     }
 
     int *arr = (int *)malloc(size * sizeof(int));
-    if (arr == NULL) {
-        perror("Ошибка выделения памяти");
-        return 1;
-    }
-
     srand(time(NULL));
+
     for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 100; 
+        arr[i] = rand() % 1000;
     }
 
-
-    long long sum = 0;
     clock_t start = clock();
+    long long sum = 0;
     for (int i = 0; i < size; i++) {
         sum += arr[i];
     }
     clock_t end = clock();
 
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-
-    printf("Последовательная сумма: %lld\n", sum);
-    printf("Время выполнения последовательной версии: %f секунд\n", time_spent);
+    printf("Array sum: %lld\n", sum);
+    printf("Time taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     free(arr);
     return 0;
