3dbdc965cc1edba5cd9d41cff964a62122ce336a
sergeeva_ss_pmi78 <pevkoooo@gmail.com>
2025-06-07 17:58:20 +0500

    Update serial_1mas.c

--- serial_1mas.c
+++ serial_1mas.c
@@ -2,65 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-void array_operations(double *arr1, double *arr2, double *sum, double *diff, double *prod, double *div, int size) {
-  for (int i = 0; i < size; i++) {
-    sum[i] = arr1[i] + arr2[i];
-    diff[i] = arr1[i] - arr2[i];
-    prod[i] = arr1[i] * arr2[i];
-    if (arr2[i] != 0.0) {
-      div[i] = arr1[i] / arr2[i];
-    } else {
-      div[i] = 0.0;
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Использование: %s <размер_массива>\n", argv[0]);
-    return 1;
-  }
+    if (argc != 2) {
+        printf("Usage: %s <array_size>\n", argv[0]);
+        return 1;
+    }
 
-  int size = atoi(argv[1]);
-  if (size <= 0) {
-    fprintf(stderr, "Размер массива должен быть положительным числом.\n");
-    return 1;
-  }
+    int size = atoi(argv[1]);
+    if (size <= 0) {
+        printf("Array size must be positive\n");
+        return 1;
+    }
 
-  double *arr1 = (double *)malloc(size * sizeof(double));
-  double *arr2 = (double *)malloc(size * sizeof(double));
-  double *sum = (double *)malloc(size * sizeof(double));
-  double *diff = (double *)malloc(size * sizeof(double));
-  double *prod = (double *)malloc(size * sizeof(double));
-  double *div = (double *)malloc(size * sizeof(double));
+    int *arr1 = (int *)malloc(size * sizeof(int));
+    int *arr2 = (int *)malloc(size * sizeof(int));
+    srand(time(NULL));
 
-  if (!arr1 || !arr2 || !sum || !diff || !prod || !div) {
-    fprintf(stderr, "Ошибка выделения памяти.\n");
-    free(arr1); free(arr2); free(sum); free(diff); free(prod); free(div);
-    return 1;
-  }
+    for (int i = 0; i < size; i++) {
+        arr1[i] = rand() % 1000 + 1;
+        arr2[i] = rand() % 1000 + 1;
+    }
 
+    clock_t start = clock();
+    for (int i = 0; i < size; i++) {
+        int sum = arr1[i] + arr2[i];
+        int diff = arr1[i] - arr2[i];
+        int prod = arr1[i] * arr2[i];
+        float div = (float)arr1[i] / arr2[i];
+    }
+    clock_t end = clock();
 
-  srand(time(NULL));
-  for (int i = 0; i < size; i++) {
-    arr1[i] = (double)rand() / RAND_MAX; 
-    arr2[i] = (double)rand() / RAND_MAX; 
-  }
+    printf("Operations completed\n");
+    printf("Time taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-  clock_t start_time = clock();
-  array_operations(arr1, arr2, sum, diff, prod, div, size);
-  clock_t end_time = clock();
-
-  double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-  printf("Последовательное выполнение: Размер массива = %d, Время = %f секунд\n", size, elapsed_time);
-
-
-  free(arr1);
-  free(arr2);
-  free(sum);
-  free(diff);
-  free(prod);
-  free(div);
-
-  return 0;
+    free(arr1);
+    free(arr2);
+    return 0;
 }
